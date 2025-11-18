2f304ec22ac2c3e0299b61047d02972f8b9169e1
iinshot <ernest_s@bk.ru>
2025-11-18 10:00:22 +0300

    refactor: add comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
+// Главная функция
 int main(){
-    std :: string name;
-    std :: cout << "Enter your name" << endl;
+    std :: string name; // Переменная для хранения имени
+    // Запрашиваем ввод у пользователя
+    std :: cout << "Enter your name" << std :: endl;
+    // Ввод пользователя
     std :: cin >> name;
-    std :: cout << "Hello World" << name << endl;
+    // Вывод приветственного сообщения
+    std :: cout << "Hello World" << name << std :: endl;
+    // Программа завершена успешно
     return 0;
 }
